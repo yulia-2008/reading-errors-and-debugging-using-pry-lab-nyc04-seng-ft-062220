1d10cedf4beb8a23e6bffa990b8fd941bf8fc27f
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-14 16:31:19 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,10 @@
-# don't forget to add: require 'pry'
+require 'pry' # don't forget to add: require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+     "ssssssssss" + string
   else
     string
+    
   end
 end
--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,15 +1,17 @@
-# don't forget to add: require 'pry'
+require 'pry'# don't forget to add: require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
+  binding.pry 
 end
 
 
 def state_log(star_date)
-  "Captain's Log, star date #{star_date}."
+ puts "Captain's Log, star date #{star_date}."
 end
 
 def engage
-  puts state_log(date)
-  date = generate_star_date
+  puts state_log(star_date)
+  star_date = generate_star_date
+  
 end
